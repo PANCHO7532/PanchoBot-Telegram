2802b62979714764a98033723f8630ab4597fc23
PANCHO7532 <pancho7532@protonmail.com>
2020-08-21 01:24:21 -0300

    P7RND_1101350

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
+/*
+* ----
+* PanchoBot for Telegram v2.7b
+* Copyright PANCHO7532 - P7COMunications LLC 2020
+* ----
+* This software is distributed under a LICENSE file that you must read if you want to do modifications
+* or code redistribution/packaging.
+*/
 const telegramApi = require('node-telegram-bot-api');
 const botToken = "YOUR_TELEGRAM_TOKEN_HERE";
+const botUsername = "YOUR_BOT_USERNAME";
 const scapikey = "YOUR_SOUNDCLOUD_APIKEY_HERE";
 const bot = new telegramApi(botToken, {polling: true});
 function fetchSoundcloud(sclink) {
@@ -16,16 +25,20 @@ function fetchSoundcloud(sclink) {
     }
 }
 bot.on('message', function(message) {
-    if(message.text == "/start") {
-        bot.sendMessage(message.chat.id, "Commands:\r\n/ping - Check your approximate ping from your location to the bot server.\r\n/dlsc + link - Download a soundcloud song.");
+    if(message.text == "/start" || message.text == "/start@" + botUsername) {
+        bot.sendMessage(message.chat.id, "Commands:\r\n/ping - Check your approximate ping from your location to the bot server.\r\n/dlsc + link - Download a SoundCloud song.");
     }
-    if(message.text == "/ping") {
+    if(message.text == "/ping" || message.text == "/ping@" + botUsername) {
         bot.sendMessage(message.chat.id, "Pong! (" + (Date.now() - message.date*1000) + " ms)", {reply_to_message_id: message.message_id});
     }
-    if(message.text.substring(0, 5) == "/dlsc") {
+    if(message.text.substring(0, 5) == "/dlsc" || message.text == "/dlsc@" + botUsername) {
+        if(!message.text.split(" ")[1]) {
+            bot.sendMessage(message.chat.id, "You may not have provided an link to your command, remember that you need to send the following (for example):\r\n/dlsc https://soundcloud.com/artistname/songtitle", {reply_to_message_id: message.message_id});
+            return;
+        }
         var sclink = fetchSoundcloud(message.text.split(" ")[1]);
         if(sclink == -1) {
-            bot.sendMessage(message.chat.id, "Song not found in Souncloud servers.", {reply_to_message_id: message.message_id});
+            bot.sendMessage(message.chat.id, "Song not found in Souncloud servers. Please check your link and make sure that it's an SoundCloud link.", {reply_to_message_id: message.message_id});
             return;
         } else if(sclink == -2) {
             bot.sendMessage(message.chat.id, "The provided link isn't a direct link to an SoundCloud song.", {reply_to_message_id: message.message_id});

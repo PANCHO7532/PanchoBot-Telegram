360ad461360d82b9a6241e1d9acd436ce7a5de10
PANCHO7532 <pancho7532@protonmail.com>
2020-08-21 00:52:25 -0300

    P7RND_530250

--- upload.sh
+++ upload.sh
@@ -3,7 +3,13 @@
 # It may not be required or relevant for other projects where this file is spotted
 # You may ask, "why not ignore this file from uploading to GitHub or any git", well... if someone ever wonder why
 # this project has strange commit names, then, this is the reason.
-git add .
-git commit -m "P7RND_$(($RANDOM * 50))"
-git push origin master
-echo "[INFO] - Upload script done!"
+echo "[INFO] - Pushing to remote repo, accept? REMEMBER TO REMOVE EVERY TOKEN AND SENSITIVE INFO FFS"
+read -p "[PROMPT] - Write \"yes\" but separated if you're ready: " ANSW
+if [ "$ANSW" == "y e s" ]; then
+    git add .
+    git commit -m "P7RND_$(($RANDOM * 50))"
+    git push origin master
+    echo "[INFO] - Upload script done!"
+else
+    echo "[ERROR] - Aborting..."
+fi
